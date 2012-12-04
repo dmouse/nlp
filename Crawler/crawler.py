485a7d6e5723b6437a48503916f5f68af3770fd8
David Flores <dmousex@gmail.com>
2012-12-03 20:23:58 -0600

    orden de archivos

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -1,105 +1,99 @@
 import time 
-import nltk # NLP 
+#import nltk # NLP 
+import hashlib
 
 from spider import Spider     # Clase para visitar los sitios web
 from datastorage import Stock # Interactua con mongodb
 
 from unidecode import unidecode
 
-url = "http://www.cs.buap.mx"
-
-unsupported = ['jpg','png','ppt','mailto','gif','xls','rar','zip','tar.gz','gz', 
-'digg','facebook','del','stumbleupon','google','twitter','linkedin','yahoo','faves']
-
-
 stop = True
 db = Stock() # instancia para almacenamiento
 
-while ( stop ):
+if (not db.count()):
+	db.save_data({'visit':False,'url':''});
+
+while( stop ):
+	break
+	if ( not db.url() ):
+		break
 
 	site = db.url()     # obtenemos una url no visitada 
 	url  = site['url']  # separo la url
+	m    = hashlib.sha1()
+	date = time.strftime("%Y-%m-%d %H:%m")
 
-
-	print "visitando: " + url 
-
-	page = db.exist_url( url )
-	
-	if ( any( tipo in url for tipo in unsupported) ): # checa si es un elemento no soportado
-		page['visit'] = True  # dice que ya lo visito
-		db.update(page)       # y hace update y continua con el siguiente link
-		continue
-
+	print "[ Visit  ] " + url 
 
 	response = Spider.get_source(url) # obtiene el html de la url
 
 
 	if not response :         #si no hay respuesta lo marca como visitado
-		page['visit'] = True  
-		#TODO : guardar el error por el cual no se pudo visitar la pagina
-		db.update(page)      # updatea el url 
+		site['visit'] = True
+		db.update(site)	    # updatea el url 
 		continue			  # y continua
 
+	if 'application/pdf' in response.headers['content-type'] :
 
-	links = Spider.get_links( response ) # obtiene la lista de links dentro del html 
-
-	#
-	# Inicializacion de variables
-	#
-
-	if response.headers['content-type'] == 'application/pdf':
 		text = Spider.pdf2Text(response.content)
 		html = ''
-	else:
-		text = unidecode( Spider.html2text(response.html) ) 
+
+		m.update(text) 
+		checksum = m.hexdigest()
+
+		if db.checksum(checksum):
+			site['visit'] = True
+			db.update(site)	    # updatea el url 
+			continue
+
+		#print '[ PDF ]'
+
+	elif 'text/html' in response.headers['content-type'] :
+
 		html  = response.html
-		print response.headers['content-type']
-		break
 
-	visit = True 
-	#token = nltk.word_tokenize( text )
-	date  = time.strftime("%Y-%m-%d %H:%m")
+		m.update(html)
+		checksum = m.hexdigest()
 
-	# update del valores
+		if db.checksum(checksum):
+			site['visit'] = True
+			db.update(site)	    # updatea el url 
+			continue
 
-	# TODO: Reference update
+		text =  Spider.html2text(response.html) 
 
-	site['visit']  = visit
-	site['text']   = text
-	site['html']   = html
-	#site['tokens'] = token
-	site['date']   = date
-	
+		links = Spider.get_links( response ) # obtiene la lista de links dentro del html 
+
+		for link in links: 
+			link = unidecode( link )
+			if not db.exist_url( link ):
+				save = {
+					'url'      : link    ,
+					'reference': [ url ] , # referencia con id
+					'text'     : ''      ,
+					'html'	   : ''      ,
+					'visit'	   : False   ,
+					'date'     : date    ,
+				}
+				db.save_data(save); # Guarda el link nuevo
+				print "[  Save  ] " + link
+
+		#print '[ html ]'
+
+	else :
+		site['visit'] = True
+		db.update(site)      	# updatea el url 
+		#print '[ VS ]'
+		continue			  	# y continua
+
+	site['visit']    = True
+	site['text']     = text
+	site['html']     = html
+	site['date']     = date
+	site['checksum'] = checksum
+
 	db.update ( site )   # update de la url visitada
-	print "[ Update ] : " + url
-
-	# Ahora guardamos los liks que obtuvimos en la pagina
-
-	for link in links: 
-
-		link = unidecode( link )
-
-		# Si no existe en la base de datos y el formato es soportado 
-		if ( not db.exist_url( link ) and ( not ( any(tipo in link for tipo in unsupported) ) ) ) : 
-
-			save = {
-				'url'      : link    ,
-				'reference': [ url ] ,
-				'text'     : ''      ,
-				'html'	   : ''      ,
-	#			'tokens'   : []      ,
-				'visit'	   : False   ,
-				'date'     : date    ,
-			};
-
-			db.save_data(save); # Guarda el link nuevo
-			
-			print "[  Save  ] : " + link
-
-	#print time.localtime();
-	#time.sleep(2);
+	print "[ Update ] " + url
 
 	if ( not db.url() ):
 		stop = False
-
-	#stop += 1
